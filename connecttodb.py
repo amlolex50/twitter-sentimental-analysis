7ab9288fdaa6cf45440537990d9f67f2dd379ed1
amlolex50 <30842683+amlolex50@users.noreply.github.com>
2018-10-24 00:34:48 +0100

    Add files via upload
    code for connection to db instance

---
+++ connecttodb.py
@@ -0,0 +1,21 @@
+import psycopg2
+import sys
+
+
+def main():
+    # Define our connection string
+    conn_string = "host='localhost' dbname='twitterdb' user='postgres' password='Ndubueze50'"
+
+    # print the connection string we will use to connect
+    print('Connecting to database')
+
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+    print("Connected!\n")
+
+
+if __name__ == "__main__":
+    main()
